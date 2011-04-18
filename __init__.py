0ebd1274463fd3bec47d9a622b817c1b3662a83a
Dave Abrahams <dave@boostpro.com>
2011-04-18 10:15:31 -0400

    Clean up comment

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@
 # master.cfg.  We don't want those droppings polluting this source
 # tree.  So instead of running buildbot directly on master.cfg, we run
 # it on a symlink living in our parent directory.  When master.cfg
-# loads bbproto, therefore, python finds this directory.
+# loads bbproto, python finds this file.
 #
 # However, when we run buildbot checkconfig directly from this
 # directory for testing purposes, master.cfg still needs to be able to
